98df12fba6dafbd8c9f1d92ad2e33fd7895a68a0
Chandramouli419 <58340457+Chandramouli419@users.noreply.github.com>
2023-03-13 16:50:39 -0400

    Create aks_veriables.tf

---
+++ aks_veriables.tf
@@ -0,0 +1,12 @@
+variable "adminuser" {
+  type = string
+  description = "user name"
+  default = "azuser"
+}
+
+
+variable "password" {
+  type = string
+  description = "password"
+  default = "myK8cluster"
+}
