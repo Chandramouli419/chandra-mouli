fc9539637e42780d2be726b41bc3ef5e641ecd73
Chandramouli419 <58340457+Chandramouli419@users.noreply.github.com>
2023-06-05 15:41:54 -0400

    Update servicebus.tf

--- servicebus.tf
+++ servicebus.tf
@@ -15,14 +15,16 @@ variable "servicebus_namespace_name" {
   type        = string
 }
 
-variable "topic_name" {
-  description = "Name of the Service Bus topic"
-  type        = string
+variable "topic_names" {
+  description = "List of topic names"
+  type        = list(string)
+  default     = []
 }
 
-variable "queue_name" {
-  description = "Name of the Service Bus queue"
-  type        = string
+variable "queue_names" {
+  description = "List of queue names"
+  type        = list(string)
+  default     = []
 }
 
 # Define Azure provider
@@ -30,24 +32,32 @@ provider "azurerm" {
   features {}
 }
 
+# Create Azure resource group
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
 # Create Azure Service Bus namespace
 resource "azurerm_servicebus_namespace" "example" {
   name                = var.servicebus_namespace_name
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.example.name
   sku                 = "Standard"
 }
 
-# Create a Service Bus topic
+# Create Service Bus topics
 resource "azurerm_servicebus_topic" "example" {
-  name                = var.topic_name
+  count               = length(var.topic_names)
+  name                = var.topic_names[count.index]
   namespace_name      = azurerm_servicebus_namespace.example.name
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-# Create a Service Bus queue
+# Create Service Bus queues
 resource "azurerm_servicebus_queue" "example" {
-  name                = var.queue_name
+  count               = length(var.queue_names)
+  name                = var.queue_names[count.index]
   namespace_name      = azurerm_servicebus_namespace.example.name
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.example.name
 }
